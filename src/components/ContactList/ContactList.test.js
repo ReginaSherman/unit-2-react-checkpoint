0740ffc54a2a01aa0e24632ac08f10b85eca21f2
jmeade11 <hobokenjam@yahoo.com>
2020-07-27 08:59:11 -0400

    Update after moving contacts.json
    Moved contacts.json so students wouldn't be asked to follow
    an antipattern of putting props in state.  Also gives students
    a chance to practice with fetch.  Updated the index.js to remove
    contacts.json and mocked the contacts that were being imported
    directly into the contact list test.

--- src/components/ContactList/ContactList.test.js
+++ src/components/ContactList/ContactList.test.js
@@ -1,19 +1,35 @@
-import React from "react";
-import { shallow } from "enzyme";
-import ReactDOM from "react-dom";
-import ContactList from "./ContactList";
-import Contact from "../Contact/Contact";
+import React from 'react';
+import { shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
+import ContactList from './ContactList';
+import Contact from '../Contact/Contact';
 
-import contacts from "../../contacts.json";
+const contacts = [
+  {
+    name: 'Tweety',
+    email: 'tweety@gmail.com',
+    image: 'https://upload.wikimedia.org/wikipedia/en/0/02/Tweety.svg',
+  },
+  {
+    name: 'Bugs Bunny',
+    email: 'bugs@gmail.com',
+    image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Bugs_Bunny.svg',
+  },
+  {
+    name: 'Daffy Duck',
+    email: 'daffy@gmail.com',
+    image: 'https://upload.wikimedia.org/wikipedia/en/f/f4/Daffy_Duck.svg',
+  },
+];
 
-describe("ContactList component:", () => {
-  it("renders without crashing", () => {
+describe('ContactList component:', () => {
+  it('renders without crashing', () => {
     shallow(<ContactList contacts={contacts} />);
   });
 
-  it("has a `.contact-list` class", () => {
+  it('has a `.contact-list` class', () => {
     const component = shallow(<ContactList contacts={contacts} />);
-    expect(component.hasClass("contact-list")).toEqual(true);
+    expect(component.hasClass('contact-list')).toEqual(true);
   });
 
   it(`should contain ${contacts.length} contacts`, () => {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App/App';
 
-import contacts from "./contacts.json";
-
 ReactDOM.render(<App />, document.getElementById('root'));
